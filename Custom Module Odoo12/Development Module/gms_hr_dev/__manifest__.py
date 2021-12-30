c3cd5f274d9414d1ee8d93910142cee4c90791d8
kayroeuy <roeuykaykaya@gmail.com>
2021-12-30 08:52:49 +0700

    gms_hr_dev

---
+++ Custom Module Odoo12/Development Module/gms_hr_dev/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "GMS Module Custom",
+    'summary': "GMS Module Custom Summary",
+    'description': "GMS Description",
+    'depends':[
+        'base'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/gms_view.xml',
+        'views/menu_config.xml',
+    ],
+    'images': ['static/description/GMS_logo.png'],
+}
